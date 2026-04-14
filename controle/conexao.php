2cdb5b003f5fe62ed821ec5a31bf9231e6a9c3e2
Leleh-Silva03 <leticiasilvarocha12@gmail.com>
2026-04-14 12:37:05 -0300

    Aula dia 14/04/26

--- controle/conexao.php
+++ controle/conexao.php
@@ -1,13 +1,42 @@
-<?php 
-/*Datamodulo do Delphi ou do Lazarus*/
-class Conexao 
+<?php
+//DataModulo do Delphi ou do Lazarus
+class Conexao
 {
-    /*FDConnection do Delphi ou ZeosConnection do Lazarus*/
+    //FDConnection do Delphi ou ZeosConnection Lazarus
     private static $dbname = "olojinha";
     private static $host = "127.0.0.1"; //localhost
-    private static $user = "hoot";
+    private static $user = "root";
     private static $pass = "";
     private static $con = null;
 
+
+public function __construct()
+{
+    throw new \Exception('Not implemented');
 }
+
+//Cria a função que conecta no banco de dados
+public static function conectar () {
+    if(null===self::$con) {
+        //1 igual é recebe (:=recebe no delphi ou lazarus)
+        //php 2 iguais é igual sem fazer diferença de caracteres (= igual no delphi ou lazarus)
+        //php 3 iguais é igual fazendo diferenças de caracteres - obs. tem que ser exatamente igual (no delphi ou lazarus)
+
+        try{
+            self::$con = new PDO ("mysql:host=" .self::$host.";dbname=" .self::$dbname, self::$user, self::$pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        } 
+
+        catch (exception $erro){
+            die ($erro->getMessage()); //possível erro de banco, tabela, atributo retorna a mensagem direta do banco
+        }
+    }
+    return self::$con;
+}
+
+public static function desconectar ()
+    {
+        self::$con = null;
+    }
+}
+
 ?>
--- menu.php
+++ menu.php
@@ -1,33 +1,52 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.html">Logo da Lojinha </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.html">Inicial</a>
+<?php
+//mesma coisa que o uses do delphi ou lazarus (ALT+F11)
+require './controle/conexao.php';
 
-          <li class="nav-item dropdown">
+//FDQuery do Delphi ou ZQuery do Lazarus
+//Conectamos a Query
+$pdo = conexao::conectar();
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "select * from categorias"; //Propriedade sql da query do Delphi / lazarus
+$prp = $pdo->prepare($sql); //Seria o princípio logico de preparação para o  execsql ou open da query no delphi/lazarus
+$prp ->execute(); //execsql ou open da query no delphi/lazarus
+?>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">LogoDaLojinha</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html"
+                  >Inicial</a>
+              </li>
+              <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Categorias
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+           <?php  while ($data = $prp->fetch(PDO::FETCH_ASSOC)) { //data é igual a datasourse do Delphi/Lazarus ?>
+            <li><a class="dropdown-item" href="listar_produtos.php?op=cat&id="><?php echo $data ['catnome']; ?></a></li>
+          <?php  }  ?>
           </ul>
         </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="SubCategorias.html">Sub Categorias</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">final</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>  
+              <li class="nav-item">
+                <a class="nav-link" href="subcategorias.html">Subcategorias</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
